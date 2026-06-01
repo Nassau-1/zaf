1230b1f7e57fba6f2611793ebcdb6bc557a5a79a
Nassau-1 <247572342+Nassau-1@users.noreply.github.com>
2026-06-01 08:15:47 +0000

    ⚡ Bolt: Unblock extension thread by using async file I/O for decorations
    Refactored `updateDecorations` to be asynchronous, mapping over ticket
    files concurrently with `fs.promises.readFile` and `Promise.all` instead
    of blocking the thread with `fs.readFileSync`. This severely cuts down
    Event Loop lag during VSCode file active operations. Removed the temporary
    `test_vscode_api.js` script.

--- test_vscode_api.js
+++
@@ -1,7 +0,0 @@
-// VS Code extension API is available in actual VS Code extension runtime environment
-// Here we are testing the rationale provided in the prompt
-
-// "Refactor loop to map file names to Promises using vscode.workspace.fs.readFile or fs.promises, and await Promise.all."
-
-// We will modify the code to use fs.promises.readFile and await Promise.all for performance scaling
-// event if there might be some overhead when simulated.
